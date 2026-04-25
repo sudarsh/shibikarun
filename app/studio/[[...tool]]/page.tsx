28e1a8232ae6069d0766f64bfda1f2f71d09cb3c
Sudarsh <sudarsh.ms@gmail.com>
2026-04-25 17:36:23 +0530

    Fix: rename dynamic import to avoid conflict with Next.js dynamic export

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -1,8 +1,8 @@
-import dynamic from "next/dynamic";
-
-const Studio = dynamic(() => import("./Studio"), { ssr: false });
+import nextDynamic from "next/dynamic";
 
 export const dynamic = "force-dynamic";
+
+const Studio = nextDynamic(() => import("./Studio"), { ssr: false });
 
 export default function StudioPage() {
   return <Studio />;
